0f4b50415c053615b23905b724a1d659e907b38d
Laura Legat <laura.legat11@gmail.com>
2020-10-18 23:23:49 +0200

    index file exchanged, test files added

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,36 @@
-import React from "react";
-import { GetServerSideProps } from "next";
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
-type Props = {
-  name: string;
-};
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Our web app</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
 
-const Home: React.FunctionComponent<Props> = ({ name }) => (
-  <div>
-    <p>Hello {name}!</p>
-  </div>
-);
-export default Home;
+      <main className={styles.main}>
+        <h1 className={styles.title}>
+          Welcome to <a>our web app</a>
+        </h1>
 
-export const getServerSideProps: GetServerSideProps<Props> = async ({
-  query,
-}) => {
-  const name = query.name instanceof Array ? query.name.join(", ") : query.name;
-  return {
-    props: {
-      name: name || "World",
-    },
-  };
-};
+        <div className={styles.grid}>
+          <div className={styles.card}>
+            <h3>Made by</h3>
+            <p>Laura Legat, Florian Federhofer & Alexander Rathbauer</p>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Deployed as</h3>
+            <p>A multi-container Docker application using Docker-compose</p>
+          </div>
+
+          <div className={styles.card}>
+            <h3>Using</h3>
+            <p>Jenkins for continuous integration and development</p>
+          </div>
+        </div>
+      </main>
+    </div>
+  );
+}
