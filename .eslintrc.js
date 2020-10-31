9052ce01832e1e756ff2ca0d4e1227ea0eb05936
Laura Legat <laura.legat11@gmail.com>
2020-10-31 01:46:28 +0100

    Fix linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,13 +1,1 @@
-module.exports = {
-    "settings": {
-        "react": {
-            "version": "detect"
-        }
-    },
-    "env": {
-        "browser": true,
-        "node": true,
-        "es6": true
-    },
-    "parser": "babel-eslint"
-};
+module.exports = {};
